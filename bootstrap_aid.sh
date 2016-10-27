2c6e40f342724a72fa5340ecd066656029c81967
jamesprime <jamesprimephd@gmail.com>
2016-10-27 23:31:43 +0100

    Updated bootstrap_aid.sh

--- bootstrap_aid.sh
+++ bootstrap_aid.sh
@@ -1,32 +1,38 @@
 #!/bin/bash
 
+if [ "$#" -eq "1" ]; then
+	os="$1"
+else
+	os="arch"
+fi
+
 function link_files() {
 	if [[ "$#" -eq "1" ]]; then
 		dir="$1"
 	else
-		dir="$HOME/.dotfiles/arch"
+		dir="$HOME/.dotfiles/$os"
 	fi
 	for file in "$dir/\.[a-zA-Z]"*; do
 		# Checks if given file is not a directory, isn't a link, and is a regular file
-		if [[ ! -d "$file" && ! -L "$(basename $file)" && -f "$file" ]]; then
+		if [[ ! -d "$file" && ! -L "$(basename "$file")" && -f "$file" ]]; then
 		# If so, copies the file into .dotfiles/arch directory, so we have most updated version where we want to link from
-			cp "$HOME/$(basename $file)" "$dir/"
+			cp "$HOME/$(basename "$file")" "$dir/"
 		fi
 		# Forces symlink from .dotfiles/arch files into $HOME directory
-		ln -sf "$file" "$HOME/$(basename $file)"
+		ln -sf "$file" "$HOME/$(basename "$file")"
 	done
 }
 
 # Checks if given file is not a directory, isn't a link, and is a regular file
-for file in "$HOME/.dotfiles/arch/\.[a-zA-Z]"*; do
-	if [[ ! -d "$file" && ! -L "$(basename $file)" && -f "$file" ]]; then
+for file in "$HOME/.dotfiles/$os/\.[a-zA-Z]"*; do
+	if [[ ! -d "$file" && ! -L "$(basename "$file")" && -f "$file" ]]; then
 		# If so, copies the file into .dotfiles/arch directory, so we have most updated version where we want to link from
-		cp "$HOME/$(basename $file)" "$HOME/.dotfiles/arch/"
+		cp "$HOME/$(basename "$file")" "$HOME/.dotfiles/$os/"
 	fi
 	if [[ -d "file" ]]; then
 		mkdir "$HOME/$file"
 		link_files "$file"
 	fi
 	# Forces symlink from .dotfiles/arch files into $HOME directory
-	ln -sf "$file" "$HOME/$(basename $file)"
+	ln -sf "$file" "$HOME/$(basename "$file")"
 done
