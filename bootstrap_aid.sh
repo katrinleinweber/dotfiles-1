a39a5d0bf6714f5328006237a9473591204dad42
jamesprime <jamesprimephd@gmail.com>
2016-10-29 19:00:43 +0100

    Updated bootstrap_wip.sh

--- bootstrap_aid.sh
+++ bootstrap_aid.sh
@@ -15,24 +15,24 @@ function link_files() {
 	for file in "$dir/\.[a-zA-Z]"*; do
 		# Checks if given file is not a directory, isn't a link, and is a regular file
 		if [[ ! -d "$file" && ! -L "$(basename "$file")" && -f "$file" ]]; then
-		# If so, copies the file into .dotfiles/arch directory, so we have most updated version where we want to link from
-			cp "$HOME/$(basename "$file")" "$dir/"
+		# If so, copies the file into .dotfiles/$os directory and append the filename with "_old", so we have the version that was already there backed up before doing anything else
+			cp "$HOME/$(basename "$file")" "$dir/$(basename "$file")_old"
 		fi
-		# Forces symlink from .dotfiles/arch files into $HOME directory
+		# Forces symlink from .dotfiles/$os files into $HOME directory
 		ln -sf "$file" "$HOME/$(basename "$file")"
 	done
 }
 
-# Checks if given file is not a directory, isn't a link, and is a regular file
 for file in "$HOME/.dotfiles/$os/\.[a-zA-Z]"*; do
+	# If given file is not a directory, isn't a link, and is a regular file, copy the file into .dotfiles/$os directory and append the filename with "_old", so we have the version that was already there backed up before doing anything else
 	if [[ ! -d "$file" && ! -L "$(basename "$file")" && -f "$file" ]]; then
-		# If so, copies the file into .dotfiles/arch directory, so we have most updated version where we want to link from
-		cp "$HOME/$(basename "$file")" "$HOME/.dotfiles/$os/"
+		cp "$HOME/$(basename "$file")" "$HOME/.dotfiles/$os/$(basename "$file")_old"
 	fi
+	# If given file is a directory, make the corresponding directory path in the home directory
 	if [[ -d "file" ]]; then
-		mkdir "$HOME/$file"
+		mkdir -p "$HOME/${file##*os/}"
 		link_files "$file"
 	fi
-	# Forces symlink from .dotfiles/arch files into $HOME directory
+	# Forces symlink from .dotfiles/$os files into $HOME directory
 	ln -sf "$file" "$HOME/$(basename "$file")"
 done
